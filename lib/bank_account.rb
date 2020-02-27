fc991b35f9deaf65d422108f488ceaa78a77e1a5
Katie Rodgers <katierodgers95@gmail.com>
2020-02-27 15:46:32 +0000

    feat: added transactions to bank account

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 class BankAccount
-  attr_reader :balance
+  attr_reader :balance, :transactions
+  
 
   def initialize
     @balance = 0
+    @transactions = []
   end
 
   def deposit(amount)
     @balance += amount
+    @transactions << {credit: amount, balance: @balance}
   end
 
   def withdrawal(amount)
