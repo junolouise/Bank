eaef4ca48f1affe14d017c114d27af98b0c227fb
Katie Rodgers <katierodgers95@gmail.com>
2020-02-27 12:32:57 +0000

    feat: deposit method added

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -6,4 +6,11 @@ describe BankAccount do
   it 'starts with a balance of 0' do
     expect(subject.balance).to eq(0)
   end
+
+  it "depositing 1 increases balance by 1" do
+    subject.deposit(1)
+    expect(subject.balance).to eq(1)
+  end
+
 end
+
