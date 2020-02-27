336b55db96e785505533c4b932fed8fbe012ec50
Katie Rodgers <katierodgers95@gmail.com>
2020-02-27 15:42:09 +0000

    refactor: changed method name from header to print statement

--- lib/print_statement.rb
+++ lib/print_statement.rb
@@ -6,4 +6,8 @@ class PrintStatement
   def initialize
     @transactions = []
   end
+
+  def print_statement
+    'date || credit || debit || balance'
+  end
 end
--- spec/print_statement_spec.rb
+++ spec/print_statement_spec.rb
@@ -6,4 +6,8 @@ describe PrintStatement do
   it 'contains no transactions upon initialization' do
     expect(subject.transactions).to be_empty
   end
+
+  it 'prints blank statement if there are no transactions' do
+    expect(subject.print_statement).to eq('date || credit || debit || balance')
+  end
 end
