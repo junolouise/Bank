9950d4ffef08e64a8beb6b979c9a44c9d3aa5022
Katie Rodgers <katierodgers95@gmail.com>
2020-02-27 16:07:00 +0000

    refactored

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -7,39 +7,43 @@ describe BankAccount do
     expect(subject.balance).to eq(0)
   end
 
-  it 'depositing 1 increases balance by 1' do
-    subject.deposit(1)
-    expect(subject.balance).to eq(1)
+  describe '#deposit' do
+    it 'depositing 1 increases balance by 1' do
+      subject.deposit(1)
+      expect(subject.balance).to eq(1)
+    end
+
+    it 'deposits multiple times and correctly adds balance' do
+      subject.deposit(10)
+      subject.deposit(1)
+      expect(subject.balance).to eq(11)
+    end
+
+    it 'adds a transaction to @transaction when making a deposit' do
+      subject.deposit(10)
+      expect(subject.transaction).to include({ credit: 10, balance: 10 })
+    end
   end
 
-  it 'deposits multiple times and correctly adds balance' do
-    subject.deposit(10)
-    subject.deposit(1)
-    expect(subject.balance).to eq(11)
-  end
+  describe '#withdrawal' do
+    it 'withdrawal by 2 decreases balance by 2' do
+      subject.deposit(10)
+      subject.withdrawal(2)
+      expect(subject.balance).to eq(8)
+    end
 
-  it 'adds a transaction to @transaction when making a deposit' do
-    subject.deposit(10)
-    expect(subject.transaction).to include({ credit: 10, balance: 10 })
-  end
+    it 'withdrawal multiple times and correctly minuses balance' do
+      subject.deposit(10)
+      subject.withdrawal(1)
+      subject.withdrawal(3)
+      expect(subject.balance).to eq(6)
+    end
 
-  it 'withdrawal by 2 decreases balance by 2' do
-    subject.deposit(10)
-    subject.withdrawal(2)
-    expect(subject.balance).to eq(8)
-  end
-
-  it 'withdrawal multiple times and correctly minuses balance' do
-    subject.deposit(10)
-    subject.withdrawal(1)
-    subject.withdrawal(3)
-    expect(subject.balance).to eq(6)
-  end
-
-  it 'adds a transaction to @transaction when making a withdrawal' do
-    subject.deposit(20)
-    subject.withdrawal(5)
-    expect(subject.transaction).to include({ debit: 5, balance: 15 })
+    it 'adds a transaction to @transaction when making a withdrawal' do
+      subject.deposit(20)
+      subject.withdrawal(5)
+      expect(subject.transaction).to include({ debit: 5, balance: 15 })
+    end
   end
 
   describe '#add_transaction_to_statement' do
